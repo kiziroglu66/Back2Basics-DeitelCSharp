4b1b1a1efc4ebd69de8fb8f1d35f8d6bb5be37ff
tanhan <tanhan.official@gmail.com>
2022-05-16 23:50:03 +0300

    First commit
    First commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+   class HelloWorld
+   {
+        static void Main(String[] args)
+        {
+            Console.Write("Hey, what is your name ?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello there !" +name);
+        }
+   }
+}
