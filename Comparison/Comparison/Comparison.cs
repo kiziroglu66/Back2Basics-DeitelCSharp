5e80ce96b2e7d8437d20598fd64d9cf8a045512b
tanhan <tanhan.official@gmail.com>
2022-06-08 01:01:54 +0300

    Comparison.cs app added.

---
+++ Comparison/Comparison/Comparison.cs
@@ -0,0 +1,52 @@
+﻿// Comparison.cs
+// Comparing integers using if statements, equality operators and relational operators.
+
+class Comparison
+{
+    static void Main()
+    {
+        // prompt user to enter the first integer
+        Console.Write("Please enter the first integer :");
+        int number1 = int.Parse(Console.ReadLine());
+
+        // prompt user to enter the second integer
+        Console.Write("Please enter the second integer :");
+        int number2 = int.Parse(Console.ReadLine());
+
+        // check if is equal
+        if (number1 == number2)
+        {
+            Console.WriteLine($"{number1} is equal to {number2}");
+        }
+
+        // check if not equal
+        if (number1 != number2)
+        {
+            Console.WriteLine($"{number1} is not equal to {number2}");
+        }
+
+        // check if smaller
+        if (number1 < number2)
+        {
+            Console.WriteLine($"{number1} is smaller than {number2}");
+        }
+
+        // check if smaller or equal
+        if (number1 <= number2)
+        {
+            Console.WriteLine($"{number1} is smaller or equal to {number2}");
+        }
+
+        // check if greater.
+        if (number1 > number2)
+        {
+            Console.WriteLine($"{number1} is greater than {number2}");
+        }
+
+        // check if greater than or equal
+        if (number1 > number2)
+        {
+            Console.WriteLine($"{number1} is greater than or equal to {number2}");
+        }
+    }
+}
