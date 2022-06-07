ea5883d6370f63bd02c98ef8a01b573638b5d577
tanhan <tanhan.official@gmail.com>
2022-06-07 23:34:46 +0300

    Text-displaying app added.
    Text-displaying app added.

---
+++ Welcome1/Welcome1/Welcome1.cs
@@ -0,0 +1,13 @@
+﻿// Welcome1.cs
+// Text-displaying app.
+
+using System;
+
+class Welcome1
+{
+    // Main method begins execution of C# app
+    static void Main()
+    {
+        Console.WriteLine("Welcome to C# Programming!");
+    }
+}
