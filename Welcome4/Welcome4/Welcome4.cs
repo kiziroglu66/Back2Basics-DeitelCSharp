37c7739f2ae0ccac326d442d26570498f5cdeb1d
tanhan <tanhan.official@gmail.com>
2022-06-08 00:36:31 +0300

    Welcome4.cs app added

---
+++ Welcome4/Welcome4/Welcome4.cs
@@ -0,0 +1,12 @@
+﻿// Welcome4.cs
+// Inserting content into a string with string intepolation.
+
+class Welcome4
+{
+    static void Main()
+    {
+        string person = "tanhan";
+
+        Console.WriteLine($"Welcome to C# Programming, {person}!"); // with string interpolation, we can embed string statements inside a string
+    }
+}
