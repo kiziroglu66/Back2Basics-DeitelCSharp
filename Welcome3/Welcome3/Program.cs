4ed8d8c11f7c0f4db9fa99fac0fe75717c729b46
tanhan <tanhan.official@gmail.com>
2022-06-08 00:19:21 +0300

    Welcome3.app added

---
+++ Welcome3/Welcome3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Welcome3.cs
+// Displaying multiple lines with a single statement.
+
+class Welcome3
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome\nto\nC#\nProgramming!"); // \n is an escape sequence character. It moves the cursor one line each time its used
+    }
+}
