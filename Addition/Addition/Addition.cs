f60adebb4acb1f08230d4012a5aaa666b525ec97
tanhan <tanhan.official@gmail.com>
2022-06-08 00:44:30 +0300

    Addition.cs app added.

---
+++ Addition/Addition/Addition.cs
@@ -0,0 +1,22 @@
+﻿// Addition.cs
+// Displaying the sum of two numbers input from the keyboard.
+
+class Addition
+{
+    static void Main()
+    {
+        int number1; // declare first number to add
+        int number2; // declare second number to add
+        int sum;     // declare sum of number1 and number2
+
+        Console.WriteLine("Please enter first integer :");
+        number1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please enter second integer :");
+        number2 = int.Parse(Console.ReadLine());
+
+        sum = number1 + number2;
+
+        Console.WriteLine($"The sum of {number1} and {number2} is {sum}");
+    }
+}
