13369def7537b6785c087000765ec99bf6439581
tanhan <tanhan.official@gmail.com>
2022-06-07 23:44:06 +0300

    F1 documentation hot-key comment added.

--- Welcome1/Welcome1/Welcome1.cs
+++ Welcome1/Welcome1/Welcome1.cs
@@ -11,6 +11,7 @@ class Welcome1
     // Main method begins execution of C# app
     static void Main()
     {
+        // Press F1 key when you hover over a .NET class or class method to view its dotnet documentation.
         Console.WriteLine("Welcome to C# Programming!");
     }
 }
