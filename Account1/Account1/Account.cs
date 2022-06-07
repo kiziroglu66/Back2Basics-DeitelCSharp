e327716190540ce9dd74be9407c84a333a1c7bc4
tanhan <tanhan.official@gmail.com>
2022-06-08 01:40:58 +0300

    Account1.cs app added.

---
+++ Account1/Account1/Account.cs
@@ -0,0 +1,18 @@
+﻿// Account.cs
+
+class Account
+{
+    private string name; // instance variable
+
+    // method that sets the account name in the object
+    public void SetName(string accountName)
+    {
+        name = accountName; // store the account name
+    }
+
+    // 
+    public string GetName()
+    {
+        return name; // returns name's value to this method's caller
+    }
+}
---
+++ Account1/Account1/AccountTest.cs
@@ -0,0 +1,22 @@
+﻿// AccountTest.cs
+// Creating and manipulating an Account object.
+
+class AccountTest
+{
+    static void Main()
+    {
+        // create an Account object and assign it to myAccount
+        Account myAccount = new();
+
+        // display myAccount's initial name (there isn't one)
+        Console.WriteLine($"myAccount object's initial name is {myAccount.GetName()}");
+
+        // prompt for and read the name, then put the name in the object
+        Console.Write("Please enter the name of the account :");
+        myAccount.SetName(Console.ReadLine());
+
+        // display the name stored in the myAccount object
+        Console.WriteLine($"myAccount object's name is {myAccount.GetName()}");
+
+    }
+}
