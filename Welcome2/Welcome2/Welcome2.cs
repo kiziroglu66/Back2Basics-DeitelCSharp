eff14f82084eace0573b3d63086567994598c51f
tanhan <tanhan.official@gmail.com>
2022-06-08 00:12:47 +0300

    Welcome2.cs app added

---
+++ Welcome2/Welcome2/Welcome2.cs
@@ -0,0 +1,13 @@
+﻿// Welcome2.cs
+// Displaying one line of text with multiple statements.
+
+// using System; including System namespace is unnecessary, so it is commented-out
+
+class Welcome2
+{
+    static void Main()
+    {
+        Console.Write("Welcome to "); // Prints the text. Cursor wait at the end of the text.
+        Console.WriteLine("C# Programming!"); // Start printing text where the cursor is on, prints it and the cursor moves at the beginning of the next line on the command window.
+    }
+}
